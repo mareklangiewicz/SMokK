bb5c63e5cf0b8e5007b2573a0227e553d06a07c5
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-04-18 11:48:30 +0200

    fun

--- kotlinsample/build.gradle.kts
+++ kotlinsample/build.gradle.kts
@@ -1,17 +1,14 @@
+import org.jetbrains.kotlin.gradle.plugin.*
 import pl.mareklangiewicz.defaults.*
+import pl.mareklangiewicz.deps.*
+import pl.mareklangiewicz.utils.*
 
 plugins {
-    application
     kotlin("jvm")
+    application
 }
 
-repositories {
-    defaultRepos()
-}
-
-application {
-    mainClassName = "pl.mareklangiewicz.smokk.MainKt"
-}
+defaultBuildTemplateForJvmApp(appMainPackage = "pl.mareklangiewicz.smokk")
 
 dependencies {
     implementation(deps.rxjava3)
@@ -22,6 +19,56 @@ dependencies {
     testImplementation(deps.uspekx)
     testImplementation(project(":smokk"))
     testImplementation(project(":smokkx"))
-//    testImplementation("com.github.langara:SMokK:0.0.2")
 }
 
+// region [Kotlin Module Build Template]
+
+fun TaskCollection<Task>.defaultKotlinCompileOptions(
+    jvmTargetVer: String = vers.defaultJvm,
+    requiresOptIn: Boolean = true
+) = withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = jvmTargetVer
+        if (requiresOptIn) freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
+    }
+}
+
+// endregion [Kotlin Module Build Template]
+
+// region [Jvm App Build Template]
+
+@Suppress("UNUSED_VARIABLE")
+fun Project.defaultBuildTemplateForJvmApp(
+    appMainPackage: String,
+    appMainClass: String = "MainKt",
+    details: LibDetails = libs.Unknown,
+    withTestJUnit5: Boolean = true,
+    withTestUSpekX: Boolean = true,
+    addMainDependencies: KotlinDependencyHandler.() -> Unit = {}
+) {
+    repositories { defaultRepos() }
+    defaultGroupAndVerAndDescription(details)
+
+    kotlin {
+        sourceSets {
+            val main by getting {
+                dependencies {
+                    addMainDependencies()
+                }
+            }
+            val test by getting {
+                dependencies {
+                    if (withTestJUnit5) implementation(deps.junit5engine)
+                    if (withTestUSpekX) implementation(deps.uspekx)
+                }
+            }
+        }
+    }
+
+    application { mainClass put "$appMainPackage.$appMainClass" }
+
+    tasks.defaultKotlinCompileOptions()
+    tasks.defaultTestsOptions(onJvmUseJUnitPlatform = withTestJUnit5)
+}
+
+// endregion [Jvm App Build Template]
